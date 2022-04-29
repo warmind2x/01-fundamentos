aabe39a2f7bbad02dfcab989873ed1c269f9733a
[warmind2x] <[jgarban@outlook.com]>
2022-04-29 07:07:39 -0400

    Storybook configured

---
+++ components/BaseLink.stories.js
@@ -0,0 +1,6 @@
+export default {
+    title: 'BaseLink'
+  }
+  
+  export const NasWebsite = () => '<BaseLink />'
+  
---
+++ components/BaseLink.vue
@@ -0,0 +1,11 @@
+<template>
+  <a href="https://warmind2x.myqnapcloud.com">My NAS Link</a>
+</template>
+
+<script>
+export default {
+    name: 'BaseLink',
+
+}
+</script>
+
